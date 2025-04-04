fddd8df81a7caacd85ccbdf7b39f62d9e951f00b
asgerdur03 <asgerdur03@gmail.com>
2025-04-04 16:29:04 +0000

    kill me

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,3 +22,14 @@ main()
         process.exit(1);
     });
 
+
+/*
+
+npm install
+npx prisma db push
+npx prisma db seed
+
+# til að endursetja gagnagrunn
+npx prisma db push --force-reset
+npx prisma db seed+
+*/
